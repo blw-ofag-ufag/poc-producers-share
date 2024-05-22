f182b9a7901e839316b4e6650cbadf74c4bfc30e
Damian-Oswald <damian.oswald@protonmail.com>
2024-05-22 14:03:14 +0200

    Write table

--- lindas.R
+++ lindas.R
@@ -1,4 +1,3 @@
-
 # attach libraries to search path
 library(httr)
 library(jsonlite)
@@ -8,7 +7,7 @@ library(XML)
 source("sparql.R")
 
 # define the SPARQL query
-data = sparql("query.rq")
+data = sparql("sparql-scrips/query.rq")
 
 # convert `producerPrice` unit
 data$producerPrice = data$producerPrice * 0.01
@@ -19,22 +18,31 @@ data$date <- as.Date(paste(data$year, data$month, "1", sep = "-"))
 # sort data by `date`
 data <- data[order(data$date),]
 
-# visualize the results
-plot(consumerPrice  ~ date, data, ylab = "Price [CHF]", xlab = "Year", type = "l", lwd = 2, ylim = c(0,2))
-grid(col = 1)
-lines(producerPrice ~ date, data, col = "red", lwd = 2)
-plot(producerPrice/consumerPrice ~ date, data, lwd = 2, lty = 1, type = "l", ylim = c(0,1))
+# compute the producer's share
+data$producerShare <- 100 * data$producerPrice / data$consumerPrice
 
+# compute smoothed versions of all shares
+data |>
+  subset(select = c("consumerPrice","producerPrice","producerShare")) |>
+  ts(start = 2001, frequency = 12) |>
+  decompose() |>
+  getElement("trend") |>
+  as.data.frame() -> trend
+names(trend) <- paste0(c("consumerPrice","producerPrice","producerShare"), "Smoothed")
+data <- cbind(data, trend)
 
-TS = ts(data[,c("consumerPrice","producerPrice")], start = 2001, frequency = 12)
-D = decompose(TS)
-r = (D$trend[,2]/D$trend[,1])*100
-
-# SMOOTHED PRODUCER'S SHARE
+# Produce graphic showing the producer's share
+svg("graphics/producer-share.svg", width = 10, height = 6)
 par(mar = c(4,4,1,1))
-plot(r, ylim = c(0,100), las = 1, ylab = "Produzentenanteil [%]", xlab = "Zeit", yaxs = "i",
-     xlim = c(floor(min(time(r))),ceiling(max(time(r)))))
+plot(producerShareSmoothed ~ I(year+month/12),
+     data, type = "l",
+     las = 1, ylab = "Produzentenanteil [%]", xlab = "Zeit", yaxs = "i", xaxs = "i",
+     ylim = c(0,100), xlim = c(floor(min(data$year+data$month/12)),ceiling(max(data$year+data$month/12))))
 abline(v = seq(2000,2050,5), h = seq(0,100,20), col = "grey")
 abline(v = seq(2000,2050,1), h = seq(0,100,4), col = "grey", lwd = 0.5)
-lines(100*TS[,2]/TS[,1], lwd = 0.5)
-lines(r, lwd = 2)
+lines(producerShare ~ I(year+month/12), data, lwd = 0.5)
+lines(producerShareSmoothed ~ I(year+month/12), data, lwd = 2)
+dev.off()
+
+# Write data as machine-readable table
+write.csv(data, "producers-share.csv", row.names = FALSE)
